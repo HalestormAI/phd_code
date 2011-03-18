be0f10c7bb5dc17c9acec4371174243b11084385
Ian <31828525+HalestormAI@users.noreply.github.com>
2011-03-18 15:00:23 +0000

    Changed comment of angleError

--- not_finished/angleError.m
+++ not_finished/angleError.m
@@ -1,4 +1,5 @@
 function angle = angleError ( n1, n2 )
-% Returns the angle difference, in degrees, between n1 and n2
+% Returns the angle difference, in degrees, between two normal
+% column-vectors n1 and n2
 
 angle = (rad2deg( acos(dot(n1,n2) / ( norm(n1)*norm(n2) ) ) ) );
