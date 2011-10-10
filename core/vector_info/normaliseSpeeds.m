e90537c0f606e14b5a4fece12b077c842a878c7d
Ian <31828525+HalestormAI@users.noreply.github.com>
2011-10-10 19:30:45 +0100

    Beginning branch for update software, including reformulation from "n" based to "abc" based dist_eqn and all experiments carried out since last commit. First branch since early multi-planar experiments.

--- core/vector_info/normaliseSpeeds.m
+++
@@ -1,6 +0,0 @@
-function speeds_norm = normaliseSpeeds( speeds )
-
-    speeds_norm = speeds - min(speeds);
-    speeds_norm = speeds_norm ./ max(speeds_norm);
-
-end
--- core/vector_info/showSpeedDistribution.m
+++
@@ -1,6 +0,0 @@
-function showSpeedDistribution( fn )
-disp(strcat('Z:/PhD/year1/testscripts/c/speed_vals/',fn))
-fid = fopen(strcat('Z:/PhD/year1/testscripts/c/speed_vals/',fn))
-speeds = textscan( fid, '%f ')
-figure,
-histfit( speeds{1} )
