983e59e5d52b5b790dcafbef5948280c3f0f2d9e
Ian <31828525+HalestormAI@users.noreply.github.com>
2012-01-27 13:42:48 +0000

    Updated angle error to allow different units (radians or degrees) and to include the "fixAngle" function internally.
    Updated euclidean vs error experiment.

--- not_finished/angleError.m
+++ not_finished/angleError.m
@@ -1,5 +1,28 @@
-function angle = angleError ( n1, n2 )
+function angle = angleError( n1, n2, mirr, unit )
 % Returns the angle difference, in degrees, between two normal
 % column-vectors n1 and n2
+%
+% Input:
+%
+%   n1              The first column vector
+%   n2              The second column vector
+%  [mirr=0]         Allow mirroring (e.g. the error plot looks thus: /\).
+%  [unit='degrees'] Set the units: 'degrees' or 'radians'.
 
-angle = acosd(dot(n1,n2) / ( norm(n1)*norm(n2) ) ) ;
+
+if nargin < 4 || strcmpi(unit,'degrees')
+    cfunc = @acosd;
+    fixDiff = 90;
+elseif strcmpi(unit,'radians')
+    cfunc = @acos;
+    fixDiff = pi/2;
+else 
+    error('Invalid unit choice given');
+end
+
+angle = cfunc(dot(n1,n2) / ( norm(n1)*norm(n2) ) ) ;
+
+if nargin >= 3 && mirr == 1,
+    angle = fixDiff - abs(fixDiff - angle);
+end
+
