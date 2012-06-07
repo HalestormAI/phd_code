6e239914d00c9455c22d660783662bf5896a1a7a
ian hales <contact@ian-hales.com>
2012-06-07 23:57:30 +0100

    saved input error matrix

--- trajectory_clustering/traj_cluster_munkres.m
+++ trajectory_clustering/traj_cluster_munkres.m
@@ -1,14 +1,14 @@
 
-
+input_cost =  cell(length(imtraj),length(imtraj));
 assignment =  cell(length(imtraj),length(imtraj));
 outputcost = zeros(length(imtraj),length(imtraj));
 
 
 for i=1:length(imtraj)
     for j=i:length(imtraj)
-        cost = cluster_traj( imtraj{i},imtraj{j} );
+        input_cost{i,j} = cluster_traj( imtraj{i},imtraj{j} );
         fprintf('Done building error matrix (%d,%d)',i,j);
-        [assignment{i,j},outputcost(i,j)] = assignmentoptimal( cost );
+        [assignment{i,j},outputcost(i,j)] = assignmentoptimal( input_cost{i,j} );
         fprintf('Element (%d,%d) of (%d,%d)\n',i,j,length(imtraj),length(imtraj));
 %         row = find(assignment ~= 0);
 %         col = assignment(row);
