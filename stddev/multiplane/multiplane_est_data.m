89c0cf089842c39dcf4b0116784d25a29b6fd109
Ian <31828525+HalestormAI@users.noreply.github.com>
2012-08-14 16:49:14 +0100

    Added sliding window regions.

--- stddev/multiplane/multiplane_est_data.m
+++ stddev/multiplane/multiplane_est_data.m
@@ -1,14 +1,18 @@
-SMOOTHCOEFF    = 3;
-NEIGHBOURCOEFF = 100;
-LABELCOEFF     = 100;
-NUM_ITERATIONS = 1;
-
-MODE_ALL_PARAM = 1; % Use all regions' estimated params as hypotheses
-MODE_CLUSTER_2 = 2; % Throw all into kmeans and cluster for 2 of them
-
-
-MODE = MODE_CLUSTER_2;
-
+SMOOTHCOEFF    = 1;
+% NEIGHBOURCOEFF = 100;
+% LABELCOEFF     = 100;
+% NUM_ITERATIONS = 1;
+% 
+% MODE_ALL_PARAM = 1; % Use all regions' estimated params as hypotheses
+% MODE_CLUSTER_2 = 2; % Throw all into kmeans and cluster for 2 of them
+% 
+% WINDOW_SIZE = 100;
+% WINDOW_DISTANCE = 75;
+% 
+% 
+% 
+% MODE = MODE_CLUSTER_2;
+% 
 % multiplane_sim_data;
 % 
 % % now split all trajectories more than length 10 in half
@@ -33,53 +37,51 @@ MODE = MODE_CLUSTER_2;
 % 
 % 
 % multiplane_sim_data;
-mm = minmax([imTraj{:}]);
-regions = multiplane_gen_sliding_regions(mm, 250, imTraj, 75);
+% mm = minmax([imTraj{:}]);
+% regions = multiplane_gen_sliding_regions(mm, WINDOW_SIZE, imTraj, WINDOW_DISTANCE);
 
 %% Now assign each trajectory a plane
 
 labelling = cell(NUM_ITERATIONS,1);
-old_regions = cell(NUM_ITERATIONS,1);
 
 for iteration = 1: NUM_ITERATIONS
-    
-    %% Region based Estimation
-    output_params = cell(length(regions),1);
-    finalError    = cell(length(regions),1);
-    fullErrors    = cell(length(regions),1);
-    inits         = cell(length(regions),1);
-    for r=1:length(regions)
-    %     plane_details.trajectories = traj2imc(multiplane_trajectories_for_region( tmpTraj, regions(r) ),1,1);
-        fprintf('Region: %d\n',r);
-        if length(regions(r).traj) < 1
-            fprintf('\tNo trajectories in region\n');
-            continue;
-        end
-        plane_details.trajectories = traj2imc( regions(r).traj,1,1 );
-        [ output_params{r}, finalError{r}, fullErrors{r}, inits{r} ] = multiplane_multiscaleSolver( 1, plane_details, 3, 10, 1e-12 );
-    end
-
-    disp('ESTIMATES');
-    output_mat = cell2mat(output_params);
-    
-    if MODE == MODE_CLUSTER_2
-        hypotheses = kmeans(output_mat,2);
-    else
-        hypotheses = output_mat;
-    end
+%     
+%     %% Region based Estimation
+%     output_params = cell(length(regions),1);
+%     finalError    = cell(length(regions),1);
+%     fullErrors    = cell(length(regions),1);
+%     inits         = cell(length(regions),1);
+%     for r=1:length(regions)
+%     %     plane_details.trajectories = traj2imc(multiplane_trajectories_for_region( tmpTraj, regions(r) ),1,1);
+%         fprintf('Region: %d\n',r);
+%         if length(regions(r).traj) < 1
+%             regions(r).empty = 1;
+%             fprintf('\tNo trajectories in region\n');
+%             continue;
+%         end
+%         regions(r).empty = 0;
+%         plane_details(r).trajectories = traj2imc( regions(r).traj,1,1 );
+%         [ output_params{r}, finalError{r}, fullErrors{r}, inits{r} ] = multiplane_multiscaleSolver( 1, plane_details(r), 3, 10, 1e-12 );
+%     end
+% 
+%     disp('ESTIMATES');
+%     output_mat = cell2mat(output_params);
+%     
+%     if MODE == MODE_CLUSTER_2
+%         [~,hypotheses] = kmeans(output_mat,2);
+%     else
+%         hypotheses = output_mat;
+%     end
 
     % Output estimation details.
     % [plane_ids,confidence] = multiplane_planeids_from_traj( planes, tmpTraj );
-    disp('GROUND TRUTH');
-    anglesFromN(planeFromPoints(planes(1).camera),1,'degrees')
-    anglesFromN(planeFromPoints(planes(2).camera),1,'degrees')
-    
+%     disp('GROUND TRUTH');
+%     anglesFromN(planeFromPoints(planes(1).camera),1,'degrees')
+%     anglesFromN(planeFromPoints(planes(2).camera),1,'degrees')
+%     
     % Collect all regions' subtrajectories into big array
     regionTraj = {regions.traj}';
     subtraj = vertcat(regionTraj{:});
-    subc = traj2imc(subtraj,1,1);
-    subtraj(cellfun(@length,subc)<4) = [];
-    subc(cellfun(@length,subc)<4) = [];
     
     
     % preprocess step - convert all angles from outputmat to normals
@@ -88,12 +90,16 @@ for iteration = 1: NUM_ITERATIONS
         normals(:,o) = normalFromAngle(hypotheses(o,1),hypotheses(o,2),'degrees');
     end
     
-    % ... build  matrix of cost for subtrajectory against plane estimate
-    labelCost = NaN.*ones(size(hypotheses,1),length(subc));
+    % ... build  matrix of cost for region against plane estimate
+    labelCost = NaN.*ones(size(hypotheses,1),length(regions));
     smoothCost = zeros(size(hypotheses,1));
     for e=1:size(hypotheses,1)
-        for t=1:length(subc)
-            labelCost(e,t) = errorfunc( hypotheses(e,1:2), [1,hypotheses(e,3)], subc(t));
+        for r=1:length(regions)
+%             if regions(r).empty
+%                 labelCost(e,r) = Inf;
+%             else
+                labelCost(e,r) = sum(errorfunc( hypotheses(e,1:2), [1,hypotheses(e,3)], traj2imc(regions(r).traj,1,1)).^2);
+%             end
         end
         
         % Build smooth cost: angle between planes.
@@ -104,38 +110,19 @@ for iteration = 1: NUM_ITERATIONS
             smoothCost(f,e) = smoothCost(e,f);
         end
     end
-
-%     IM1 = mat2gray(labelCost);
-%     figure;imagesc(IM1)
-%     axis equal;
-%     axis([1 length(subc) 1 length(output_mat)]);
-%     xlabel('Plane Hypothesis');
-%     ylabel('Sub-Trajectory ID');
-%     title('Heatmap of plane hypothesis error');
-%     
-%     IM2 = mat2gray(smoothCost');
-%     figure;imagesc(IM2)
-%     axis equal;
-%     axis([1 size(smoothCost,2) 1 size(smoothCost,1)]);
-%     xlabel('Plane Hypothesis');
-%     ylabel('Plane Hypothesis');
-%     title('Heatmap of smoothness cost between hypotheses');
-%     
     
     % ... build matrix of image distance for each trajectory
-    assignment =  cell(length(subtraj),length(subtraj));
-    distanceCost = zeros(length(subtraj),length(subtraj));
-    for i=1:length(subtraj)
-        for j=i:length(subtraj)
-            if i==j
-                distanceCost(i,j) = NaN; %Can't match to itself
+    distanceCost = zeros(length(regions));
+    for r1=1:length(regions)
+        for r2=r1:length(regions)
+            if r1==r2
+                distanceCost(r1,r2) = NaN; %Can't match to itself
             else
-                input_cost = cluster_traj( subtraj{i},subtraj{j} );
-                [assignment{i,j},distanceCost(i,j)] = assignmentoptimal( input_cost );
+                distanceCost(r1,r2) = vector_dist(regions(r1).centre, regions(r2).centre);
             end
-            distanceCost(j,i) = distanceCost(i,j);
+            distanceCost(r2,r1) = distanceCost(r1,r2);
         end
-        fprintf('\tRow %d of %d done.\n', i, length(subtraj));
+        fprintf('\tRow %d of %d done.\n', r1, length(regions));
     end
     
     % normalise and convert cost to affinity
@@ -151,7 +138,7 @@ for iteration = 1: NUM_ITERATIONS
 %     title('Heatmap of image distance affinities between trajectories (red is better)');
 %     
     
-    alphaObj = GCO_Create(length(subtraj),size(hypotheses,1));
+    alphaObj = GCO_Create(length(regions),size(hypotheses,1));
     GCO_SetDataCost( alphaObj, LABELCOEFF.*labelCost );
     GCO_SetSmoothCost( alphaObj, SMOOTHCOEFF.*smoothCost );
     GCO_SetNeighbors( alphaObj, NEIGHBOURCOEFF .* distanceCost );
@@ -159,19 +146,26 @@ for iteration = 1: NUM_ITERATIONS
     labelling{iteration} = GCO_GetLabeling(alphaObj);
 
     labels = unique(labelling{iteration});
+    
+    labelling{iteration}(logical([regions.empty])) = 0;
 
     % Get the planes for each label
     planeids = NaN.*ones(length(labels),1);
     for l=1:length(labels)
-        ids = multiplane_planeids_from_traj( planes, subtraj(labelling{iteration} == labels(l)) );
-        planeids(l) = mean(ids);
+        ids{l} = multiplane_planeids_from_traj( planes, subtraj(labelling{iteration} == labels(l)) );
+        planeids(l) = mean(ids{l});
     end
     
-    old_regions{iteration} = regions;
+    history(iteration).smoothCost   = smoothCost;
+    history(iteration).distanceCost = distanceCost;
+    history(iteration).labelCost    = labelCost;
+    history(iteration).labelling    = labelling;
+    history(iteration).regions      = regions;
     clear regions;
     
-    % Get new regions as labelled trajectory segments
+    % TODO: Get new trajectory segments for new regions.
     for l=1:length(labels)
         regions(l).traj = subtraj(labelling{iteration} == labels(l));
+        regions(l).centre = mean([regions(l).traj{:}],2);
     end
 end
