2ce74c62abbf24e4dcdcaa3ee197e34e127dae8e
Ian Hales <sc06ijh@leeds.ac.uk>
2013-04-03 14:01:09 +0100

    Some funny changes with mex that should probably be ignored. More importantanly, heightvar experiment modified to run over a smaller set of planes and to run over walk speed instead.

--- mex/compile_multiplane_sim.sh
+++ mex/compile_multiplane_sim.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-mex multiplane_add_trajectories.cpp Matrix.cpp Point.cpp Plane.cpp Trajectory.cpp Line.cpp functions.cpp SimTrajectory.cpp camcal_pets/cameraModel.cpp camcal_pets/xmlUtil.cpp /usr/lib/i386-linux-gnu/libxml2.so -I/usr/include/libxml2 -I./camcal_pets -g
+INCLUDES="-I/usr/include/libxml2 -I./camcal_pets"
+SOURCES="Matrix.cpp Point.cpp Plane.cpp Trajectory.cpp Line.cpp functions.cpp SimTrajectory.cpp camcal_pets/cameraModel.cpp camcal_pets/xmlUtil.cpp /usr/lib/i386-linux-gnu/libxml2.so"
+
+CFLAGS="-g"
+
+mex $1 $INCLUDES $SOURCE $CFLAGS
